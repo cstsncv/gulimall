1b0f744cdcf0a8aa375ba2cc3af585a376b52b8f
cstsncv <cstsncv@sina.com>
2022-07-13 17:52:06 +0800

    product/category/list/tree

--- gulimall-product/src/main/java/com/cstsncv/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/cstsncv/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -41,7 +41,7 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
                 categoryEntity.getParentCid() == 0).map((menu) -> {
                     menu.setChildren(getChildrens(menu, categoryEntities));
                     return menu;}).sorted((menu1, menu2) -> {
-                        return menu1.getSort() - menu2.getSort();
+                        return (menu1.getSort()==null?0:menu1.getSort()) - (menu2.getSort()==null?0:menu2.getSort());
         }).collect(Collectors.toList());
 
         return level1Menus;
@@ -50,7 +50,17 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
     // 递归查找所有菜单的子菜单
     private List<CategoryEntity> getChildrens(CategoryEntity root, List<CategoryEntity> all) {
 
-        return null;
+        List<CategoryEntity> children = all.stream().filter(categoryEntity -> categoryEntity.getParentCid() == root.getCatId()).map(
+                (categoryEntity) -> {
+                    // 找到子菜单
+                    categoryEntity.setChildren(getChildrens(categoryEntity, all));
+                    return categoryEntity;
+                }
+        ).sorted((menu1, menu2) -> {
+            // 菜单排序
+            return (menu1.getSort()==null?0:menu1.getSort()) - (menu2.getSort()==null?0:menu2.getSort());
+        }).collect(Collectors.toList());
+        return children;
     }
 
 }
